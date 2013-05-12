b5623470a4c7e5351190c69f6b15cafcacfb0c5a
Camron Bute <camron.bute@gmail.com>
2013-05-12 09:52:08 -0600

    Seems to have password encryption working

--- core/app/spec/entities/message_spec.rb
+++ core/app/spec/entities/message_spec.rb
@@ -1,17 +1,21 @@
 require_relative '../../entities/message'
 
 describe Message do
+    before :all do
+        @valid_messages = [{ :title => 'Valid title', :body => 'Valid Body', :id => 1, :user_id => 1, :reply_to_message_id => nil }]
+        @invalid_messages = [{ :title => '', :body => 'Valid Body', :id => 1, :user_id => 1, :reply_to_message_id => nil }]
+    end
   
   describe '.populate' do
     it 'should populate with valid input' do
-    	input = { :title => 'Valid title', :body => 'Valid Body', :id => 1, :user_id => 1, :reply_to_message_id => nil }
+    	input = @valid_messages.first
     	message = Message.new
     	message.populate input
-    	message.to_hash.should eq :title => 'Valid title', :body => 'Valid Body', :id => 1, :user_id => 1, :reply_to_message_id => nil
+    	message.to_hash.should eq @valid_messages.first
     end
 
     it 'should raise an error with invalid input' do
-    	input = { :title => '', :body => 'This has invalid title', :id => 1, :user_id => 1, :reply_to_message_id => nil }
+    	input = @invalid_messages.first
     	message = Message.new
     	expect{ message.populate input }.to raise_error Validation::InvalidWritingError
     end
@@ -20,11 +24,10 @@ describe Message do
     
   describe '.to_hash' do
     it 'should to_hash with valid input' do
-    	input = { :title => 'Valid title', :body => 'Valid Body', :id => 1, :user_id => 1, :reply_to_message_id => nil }
+    	input = @valid_messages.first
     	message = Message.new
     	message.populate input
-    	message.to_hash.should eq :title => 'Valid title', :body => 'Valid Body', :id => 1, :user_id => 1, :reply_to_message_id => nil
+    	message.to_hash.should eq @valid_messages.first
     end
-  end
-    
+    end
 end
