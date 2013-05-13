154a2300aa60a7ed6413abaccdc6a80ab06dc7f8
Camron Bute <camron.bute@gmail.com>
2013-05-13 09:51:14 -0600

    Finalized user creation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,13 @@
+require_relative '../core/app/actions/add_user'
+require_relative '../core/app/entities/entity_factory'
+require_relative '../core/external/bcrypt_plug'
+require_relative '../core/external/sample_user_plug'
+
 class UserController < ApplicationController
-  def create
+  def create input
+  	action = AddUser.new SampleUserPlug.new, BCryptPlug.new, EntityFactory.new
+  	action.execute input
+
+  	redirect_to list_messages_path
   end
 end
---
+++ core/external/sample_user_plug.rb
@@ -0,0 +1,5 @@
+class SampleUserPlug
+	def create input
+		nil
+	end
+end
