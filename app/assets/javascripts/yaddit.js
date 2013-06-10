0bc224e1dc7f448a6333877542d8f542e4856229
Derrick Schwabe <admin@mrschwabe.com>
2013-06-10 08:09:10 -0600

    Added UI for changing color theme.

--- app/assets/javascripts/yaddit.js
+++ app/assets/javascripts/yaddit.js
@@ -32,6 +32,30 @@ $(document).ready(function(){
 	$(".replyButton").each(function(i, obj) {
 		$(this).click(function(event) {
 			$(this).next(".replyForm").toggle(); 
-		})
-	})
+		});
+	});
+
+	//Theme changer (TODO: send new theme value to database!): 
+	$("input[name='theme']").change(function(){
+		console.log('Theme option changed.');
+		var option = $("[name='theme']:checked").val();
+
+		//Remove any existing theme styles: 
+		if ($("link[title='theme']") != 'undefined') {
+			$("link[title='theme']").remove(); 
+		}
+
+		//Then apply the appropriate theme: 
+		if(option == 'hacker') {
+			$('head').append('<link title="theme" rel="stylesheet" type="text/css" href="https://dl.dropboxusercontent.com/u/8495276/1306/yaddit/theme_hacker.css">');			
+		}
+		if(option == 'subtle') {
+			$('head').append('<link title="theme" rel="stylesheet" type="text/css" href="https://dl.dropboxusercontent.com/u/8495276/1306/yaddit/theme_subtle.css">');	
+		}
+
+		if(option == 'social') {
+			$('head').append('<link title="theme" rel="stylesheet" type="text/css" href="https://dl.dropboxusercontent.com/u/8495276/1306/yaddit/theme_social.css">');	
+		}		
+	}); 
+
 });
